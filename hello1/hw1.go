ed0d709c17bd6201a879f177b18084898ee12a8b
Alice Wercker <alice.wercker@gmail.com>
2018-11-09 11:50:03 +0000

    canary commit at 2018-11-09 11:50:03.126232027 +0000 UTC m=+3.584069222

--- hello1/hw1.go
+++ hello1/hw1.go
@@ -5,6 +5,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World at 2018-11-09 11:34:19.910582812 +0000 UTC m=+4.023496552")
+	fmt.Println("Hello World at 2018-11-09 11:50:03.12440709 +0000 UTC m=+3.582244305")
 }
 
